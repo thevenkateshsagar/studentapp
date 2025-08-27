dbadac7e07f4f3beddb70a3d163a3028684264dc
Vinodkumar <vinodkumar.deshpande90@gmail.com>
2025-08-27 23:36:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# MySQL Config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mypassword'
+app.config['MYSQL_DB'] = 'studentdb'
+
+mysql = MySQL(app)
+
+# Home - List Students
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index.html', students=data)
+
+# Add Student
+@app.route('/add', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        course = request.form['course']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students (name, email, course) VALUES (%s, %s, %s)", (name, email, course))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+# Edit Student
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_student(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students WHERE id=%s", (id,))
+    student = cur.fetchone()
+    cur.close()
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        course = request.form['course']
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE students SET name=%s, email=%s, course=%s WHERE id=%s", (name, email, course, id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', student=student)
+
+# Delete Student
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete_student(id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM students WHERE id=%s", (id,))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
